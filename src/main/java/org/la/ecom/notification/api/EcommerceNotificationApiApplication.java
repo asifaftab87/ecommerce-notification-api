f126bc3879af2e2c98b5ca3badd6f18adff3c9ec
asifaftab87 <asifaftab87@gmail.com>
2020-11-14 10:36:37 +0800

    added client notification document dto classes

--- src/main/java/org/la/ecom/notification/api/EcommerceNotificationApiApplication.java
+++ src/main/java/org/la/ecom/notification/api/EcommerceNotificationApiApplication.java
@@ -1,14 +1,14 @@
 package org.la.ecom.notification.api;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class EcommerceNotificationApiApplication {
 
-	/*
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceNotificationApiApplication.class, args);
 	}
-*/
+
 }
